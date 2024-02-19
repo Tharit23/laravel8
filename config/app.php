56bd116e83f2fed049430703b58ebdb5ce20dfa5
Tharit23 <151496431+Tharit23@users.noreply.github.com>
2024-02-19 10:42:55 +0700

    week 3

--- config/app.php
+++ config/app.php
@@ -161,6 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
+
 
         /*
          * Package Service Providers...
@@ -229,6 +232,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\MyProfileController;
 use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -91,25 +93,32 @@ Route::get("/gallery/cat", function () {
 });
 
 //week3
+Route::middleware(['auth', 'role:admin,teacher'])->group(function () {
 Route::get("/teacher" , function (){
 	return view("teacher");
 });
+});
 
+Route::middleware(['auth'])->group(function () {
 Route::get("/student" , function (){
 	return view("student");
+});
 });
 
 Route::get("/theme" , function (){
 	return view("theme");
 });
 
+
 // Route Template Inheritance
 Route::get("/teacher/inheritance", function () {
     return view("teacher-inheritance");
 });
+
 Route::get("/student/inheritance", function () {
     return view("student-inheritance");
 });
+
 
 // Route Template Component
 Route::get("/teacher/component", function () {
@@ -138,3 +147,45 @@ Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
 
 Route::get("study-question", [ QuizController::class, "question" ])->name("study-question");
 Route::post("study-match", [ QuizController::class, "match" ])->name("study-match");
+
+
+//weeek5 sda
+use App\Http\Controllers\Covid19Controller;
+use App\Http\Controllers\StaffController;
+
+Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+
+Route::get("/product/create",[ ProductController::class , "create" ]);
+Route::get("/product/{id}/edit", [ ProductController::class , "edit" ]);
+
+Route::get('/product/{id}',[ ProductController::class,'show' ]);
+
+Route::get("/product", [ProductController::class, "index"])->name('product.index');
+Route::get("/product/create", [ProductController::class, "create"])->name('product.create');
+Route::post("/product", [ProductController::class, "store"])->name('product.store');
+Route::get('/product/{id}', [ProductController::class, "show"])->name('product.show');
+Route::get("/product/{id}/edit", [ProductController::class, "edit"])->name('product.edit');
+Route::patch("/product/{id}", [ProductController::class, "update"])->name('product.update');
+Route::delete("/product/{id}", [ProductController::class, "destroy"])->name('product.destroy');
+
+// Route::resource('/product', ProductController::class );
+
+Route::post("/product",[ ProductController::class , "store" ]);
+Route::patch("/product/{id}", [ ProductController::class , "update" ]);
+
+Route::delete('/product/{id}', [ ProductController::class , 'destroy' ]);
+
+//qiuz
+Route::resource('/product',ProductController::class);
+Route::resource('/staff',StaffController::class);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+Route::resource('post', 'PostController');
+
+// Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
